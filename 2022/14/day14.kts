7041f0627169375e4780e7fbd9a101fe256a857d
Ryan Malaspina <ryan@malaspina.tech>
2022-12-14 01:29:58 -0500

    Day 14

---
+++ 2022/14/day14.kts
@@ -0,0 +1,54 @@
+import java.io.File
+
+data class Pos(val x: Int, val y: Int) {
+    fun down() = copy(y = y + 1)
+    fun downLeft() = copy(x = x - 1, y = y + 1)
+    fun downRight() = copy(x = x + 1, y = y + 1)
+}
+
+val filled = hashSetOf<Pos>()
+File("input.txt").forEachLine { line ->
+    val rockPos =  line.split(" -> ")
+    for (i in 1 until rockPos.size) {
+        val rock1 = rockPos[i - 1].split(",").map { it.toInt() }
+        val rock2 = rockPos[i].split(",").map { it.toInt() }
+
+        val xs = listOf(rock1[0], rock2[0]).sorted()
+        val ys = listOf(rock1[1], rock2[1]).sorted()
+
+        val rockPos1 = Pos(xs[0], ys[0])
+        val rockPos2 = Pos(xs[1], ys[1])
+
+        when {
+            xs.distinct().size == 1 -> {
+                for (i in ys[0]..ys[1])
+                    filled += Pos(xs[0], i)
+            }
+            ys.distinct().size == 1 -> {
+                for (i in xs[0]..xs[1])
+                    filled += Pos(i, ys[0])
+            }
+        }
+    }
+}
+
+val maxY = filled.maxBy { it.y }.y
+
+val sandSpawn = Pos(500, 0)
+
+var sandCount = 0
+var sandPos = sandSpawn.copy()
+do {
+    when {
+        !filled.contains(sandPos.down()) -> sandPos = sandPos.down()
+        !filled.contains(sandPos.downLeft()) -> sandPos = sandPos.downLeft()
+        !filled.contains(sandPos.downRight()) -> sandPos = sandPos.downRight()
+        else -> {
+            filled += sandPos.copy()
+            sandPos = sandSpawn.copy()
+            sandCount++
+        }
+    }
+} while (sandPos.y < maxY)
+
+println(sandCount)
